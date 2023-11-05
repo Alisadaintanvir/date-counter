af66444055f0b96d0ab078107b17f537c6fc5a9d
alisadaintanvir <alisadaintanvir@gmail.com>
2023-11-05 19:08:01 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
   const date = new Date();
   date.setDate(date.getDate() + count);
+  const newDate = date.toDateString();
 
   return (
     <div className="container">
@@ -36,20 +30,28 @@ export default function App() {
       <div className="component">
         <button
           className="btn"
-          onClick={() => setCount((currCount) => currCount - 1)}
+          onClick={() => setCount((currCount) => currCount - step)}
         >
           -
         </button>
         <h3>Count: {count}</h3>
         <button
           className="btn"
-          onClick={() => setCount((currCount) => currCount + 1)}
+          onClick={() => setCount((currCount) => currCount + step)}
         >
           +
         </button>
       </div>
-
-      <h3 className="result">Today is {date.toLocaleDateString()}</h3>
+      <h3 className="result">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{newDate}</span>
+      </h3>
     </div>
   );
 }
